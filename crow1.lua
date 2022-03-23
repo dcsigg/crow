752b71a242f18c29cc1b533d63df12a86be1ce1f
dcsigg <99462277+dcsigg@users.noreply.github.com>
2022-03-22 23:12:23 -0500

    Add files via upload

---
+++ crow1.lua
@@ -0,0 +1,32 @@
+--- my first crow script ... nothing too fancy,
+
+--in1: voltage source 1 (lfo or other)
+--in2: voltage source 2 (lfo or other)
+--out1: quantized voltage to oscillator
+--out1: quantized voltage to oscillator
+
+
+function init ()
+      input[1]{ mode = 'stream'
+      , time = 0.25
+      , stream = function ()
+                     v = (input[1].volts)
+                     print(v)
+
+                     output[1].volts = (math.floor(v*12)/12)
+                     --print("value v")
+                     --print(v)
+                     output[1].scale({0,2,4,5,7,9})
+                                 end
+                }
+      input[2] { mode = 'stream'
+      , time = 0.5
+      , stream = function ()
+                      w = (input[2].volts-1)--- subtract by 2 or more to get lower notes
+                      output[2].volts = (math.floor(w*12)/12)
+                      --print("value w")
+                      --print(w)
+                      output[2].scale({0,2,4,7})
+                    end
+                  }
+  end
